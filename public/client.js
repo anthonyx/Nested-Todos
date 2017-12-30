40dd934afbba049c6fbfbf91610fffeb0dadd9c9
Glitch (todoapplication) <none>
2017-12-30 21:37:23 +0000

    🏔🏈 Checkpoint public/client.js:479805 views/index.html:479805

--- public/client.js
+++ public/client.js
@@ -143,11 +143,19 @@ var view = {
     });
     var todosDiv = document.querySelector('input')
     todosDiv.addEventListener('keydown', function(event) {
-      var eventType = event.target.id
       debugger;
+      var eventType = event.target.id
+      var runFunction = function() {if(eventType === 'addTodoTextInput') {
+        return handlers.addTodo
+      } else if (eventType === 'changeTodoTextInput') {
+        return handlers.changeTodo
+      } else if (eventType === 'toggleCompletedPositionInput') {
+        return handlers.toggleCompleted
+      }
+      };
       var elementKeydown = event.keyCode;
       if (elementKeydown === 13) {
-        handlers.addTodo()
+        runFunction()
       }
     });
     
